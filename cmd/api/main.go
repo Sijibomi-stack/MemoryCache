9c5cce24d34c3831ff3ba7ad86b7b0c63f3fe192
Sijibomi-stack <sijiaderinto02new@gmail.com>
2023-02-14 17:09:57 +0100

    2 commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"time"
 
+	"github.com/Sijibomi-stack/memoryRoutes"
 	"github.com/gofiber/fiber/v2"
 )
 
 func setupRoutes(app *fiber.App) {
 
-	app.Get("/id", checkCache, getCache)
+	app.Get("/id", memoryRoutes.checkCache, memoryRoutes.getCache)
 
-	app.Post("/post", PostInCache)
+	app.Post("/post", memoryRoutes.PostInCache)
 
 }
 
@@ -19,7 +20,7 @@ func main() {
 
 	memoryCache.SetTTL(time.Duration(30 * time.Minute))
 
-	memRoutes(app)
+	memoryRoutes(app)
 
 	app.Listen(":7000")
 }
